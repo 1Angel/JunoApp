fcc35bf94d4db2482c2470393b7b51702851c48c
Angel Abreu <iangeeel01@gmail.com>
2025-11-08 17:26:25 -0400

    shadcn ui library added, pagination added to front page

--- Juno/junofe/src/components/Card.tsx
+++ Juno/junofe/src/components/Card.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import { Properties } from "@/types"
 import Carousel from "./Carousel";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface Props {
     data: Properties
@@ -11,6 +15,7 @@ const RDCurrency = Intl.NumberFormat("en-US", {
 });
 
 export default function HouseCard({ data }: Props) {
+    const router = useRouter();
     return (
         // <div className="max-w-sm rounded overflow-hidden shadow-lg">
         //     <img className="w-full" src="https://tse3.mm.bing.net/th/id/OIP.5kGyoxY2XZDixSK0JrZLjQHaFE?rs=1&pid=ImgDetMain&o=7&rm=3" alt="Sunset in the mountains" />
@@ -25,19 +30,19 @@ export default function HouseCard({ data }: Props) {
         //     </div>
         // </div>
 
-        <div className="border-0 overflow-hidden w-full shadow-2xl rounded-2xl">
-            {/* <img className="w-full" src={'https://tse2.mm.bing.net/th/id/OIP.HyPO0GQqnsGoMcauAHz_MQHaE7?rs=1&pid=ImgDetMain&o=7&rm=3'} /> */}
-            <Carousel images={data.images}/>
-            <div>
-                <span className="font-bold text-xl pl-4">RD$ {RDCurrency.format(data.price)}</span>
+            <div className="border-0 overflow-hidden w-full shadow-2xl rounded-2xl cursor-pointer" onClick={()=> router.push(`/home-details/${data.id}`)}>
+                {/* <img className="w-full" src={'https://tse2.mm.bing.net/th/id/OIP.HyPO0GQqnsGoMcauAHz_MQHaE7?rs=1&pid=ImgDetMain&o=7&rm=3'} /> */}
+                <Carousel images={data.images} />
+                <div>
+                    <span className="font-bold text-xl pl-4">RD$ {RDCurrency.format(data.price)}</span>
+                </div>
+
+                <div className="px-6 py-4">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{data.bedrooms} Bedrooms</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{data.bathrooms} Bathrooms</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{data.square_meters} M²
+                    </span>
+                </div>
             </div>
-            
-            <div className="px-6 py-4">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{data.bedrooms} Bedrooms</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{data.bathrooms} Bathrooms</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{data.square_meters} M²
-                </span>
-            </div>
-        </div>
     )
 }
