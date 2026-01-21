3df6fd22b8e28a42a54b1c842642b267508b8c0b
Angel Abreu <iangeeel01@gmail.com>
2026-01-20 21:46:30 -0400

    added property card to display 3 properties in the front page :D

--- Juno/JunoAngular/src/app/pages/HomePage/HomePage.ts
+++ Juno/JunoAngular/src/app/pages/HomePage/HomePage.ts
@@ -1,13 +1,32 @@
-import { ChangeDetectionStrategy, Component, computed, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal } from '@angular/core';
 import { AuthService } from '../../common/Services/AuthService';
 import { Map } from "../../components/Map/Map";
+import { PropertiesService } from '../../common/Services/PropertiesService';
+import { PropertyCard } from '../../components/PropertyCard/PropertyCard';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home-page',
-  imports: [Map],
+  imports: [Map, PropertyCard, RouterLink],
   templateUrl: './HomePage.html',
   styleUrl: './HomePage.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePage {
- }
+export class HomePage implements OnInit{
+  ngOnInit(): void {
+
+    this.getProperties();
+  }
+
+  private readonly service = inject(PropertiesService);
+
+  properties = signal<Properties[]>([]);
+
+  getProperties(){
+    this.service.getProperties("FOR_SALE", 1, 3, '').subscribe((res)=>{
+      this.properties.set(res.results)
+    });
+  }
+
+
+}
